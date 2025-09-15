7b0ec7390ebde1a05fd7b1d4d4224084ab93ae37
coolnithila <117049901+coolnithila@users.noreply.github.com>
2025-09-15 12:54:19 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import gatorLogo from './gator.gif';
+import appLogo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={gatorLogo} className="App-logo" alt="logo" />
-        <p> Hello World! </p>
-        <p> My name is Nithila and this is my project </p>
-      </header>
+      <img src={appLogo} className="App-logo" alt="logo" />
+      <p>Hello from teammate branch</p>
     </div>
   );
 }
